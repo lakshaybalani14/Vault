971673373b5c78f20f1338a93a299836ac6f3a3c
lakshaybalani14 <lakshaybalani1234@gmail.com>
2026-06-05 01:46:50 +0530

    fix: suppress React 19 hydration warning from next-themes script injection

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>
+      <body suppressHydrationWarning>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
